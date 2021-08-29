b14ed4e3cef5ef2b28f4bc79cacc0e6b59d53a91
Arsalan Khalid <arslankhalid889@gmail.com>
2021-08-29 04:10:29 -0700

    setting up and intializing vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,40 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    cart: {
+      items: [],
+    },
+
+    isAuthenticated: false,
+
+    token: "",
+
+    isLoading: false,
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem("cart")) {
+        state.cart = JSON.parse(localStorage.getItem("cart"));
+      } else {
+        localStorage.setItem("cart", JSON.stringify(state.cart));
+      }
+    },
+
+    addToCart(state, item) {
+      const existingItem = state.cart.items.filter(
+        (i) => i.product.id === item.product.id
+      );
+      if (existingItem.length) {
+        existingItem[0].quantity =
+          parseInt(existingItem[o].quantity) + parseInt(item.quantity);
+      } else {
+        state.cart.items.push(item);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
